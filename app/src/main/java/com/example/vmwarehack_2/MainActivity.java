41cdaa3727a66230c0c80e43181d42ed16ea45f7
Amber Wang <amberwang002@gmail.com>
2020-07-13 19:07:28 -0700

    button

--- app/src/main/java/com/example/vmwarehack_2/MainActivity.java
+++ app/src/main/java/com/example/vmwarehack_2/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.vmwarehack_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button yourButton = (Button) findViewById(R.id.button);
+        yourButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Quests.class));
+            }
+        });
     }
 }
