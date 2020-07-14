5bcd4ddd47fcb78518a13c758b3c0d12c755cf27
Justin Lee <justinwlee3.1415@gmail.com>
2020-07-13 19:45:16 -0700

    added buttons to character page

--- app/src/main/java/com/example/vmwarehack_2/Character.java
+++ app/src/main/java/com/example/vmwarehack_2/Character.java
@@ -2,7 +2,10 @@ package com.example.vmwarehack_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Character extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class Character extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character);
+
+        Button char1 = (Button) findViewById(R.id.chartochar);
+        char1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Character.this, Character.class));
+            }
+        });
+        Button char2 = (Button) findViewById(R.id.chartohome);
+        char2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Character.this, MainActivity.class));
+            }
+        });
+        Button char3 = (Button) findViewById(R.id.chartocomm);
+        char3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Character.this, Community.class));
+            }
+        });
     }
 }
