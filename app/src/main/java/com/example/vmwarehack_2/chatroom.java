4e0b7a16df5a336bb15f9a8dd64d6cd2a2cc6ada
Eileen <eileenchang1216@gmail.com>
2020-07-14 02:39:08 -0700

    perhaps the chat feature is doing something now

--- app/src/main/java/com/example/vmwarehack_2/chatroom.java
+++ app/src/main/java/com/example/vmwarehack_2/chatroom.java
@@ -1,18 +1,54 @@
 package com.example.vmwarehack_2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class chatroom extends AppCompatActivity {
+
+    private DatabaseReference myDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatroom);
 
+        myDatabase = FirebaseDatabase.getInstance().getReference("Message");
+
+        final TextView myText = findViewById(R.id.chatView);
+
+        myDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                myText.setText(snapshot.getValue().toString());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                myText.setText("CANCELLED");
+
+            }
+        });
+    }
+
+    public void sendMessage(View view){
+        EditText myEditText = findViewById(R.id.sendMessage);
+        myDatabase.setValue(myEditText.getText().toString());
+        myEditText.setText("");
     }
 }
